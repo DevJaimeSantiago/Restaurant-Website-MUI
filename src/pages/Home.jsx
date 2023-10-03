dd48aadca526a2ab1b59e1275a8f67cc7857f876
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-10-03 12:44:44 -0500

    Navbar (Responsive) Finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
+import { Link } from "react-router-dom";
+
 // Components
 import Layout from "../components/Layout/Layout";
+
+// images
+import Banner from "../images/banner.jpeg"
 
 const Home = () => {
 	return (
 		<Layout>
-			<h1>Home</h1>
+			<div className="home" style={{backgroundImage: `url(${Banner})`}}>
+				<div className="headerContainer">
+					<h1>Food Website</h1>
+					<p>Best Food In India</p>
+					<Link to="/menu">
+						<button>ORDER NOW</button>
+					</Link>
+				</div>
+			</div>
 		</Layout>
 	);
 };
